b65dd24c8469339d4092442cb92eb75e83a8aab7
Khalid <kadan@deloitte.ca>
2021-12-01 10:36:30 -0500

    updated styles; finished header and breadcrumbs

--- components/Breadcrumbs/index.tsx
+++ components/Breadcrumbs/index.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import { BreadcrumbItem } from './BreadcrumbItem'
 
-export const Breadcrumbs = () => {
-	
-	return (
-		<div></div>
-	);
-};
+export const Breadcrumbs: React.VFC<{ items: string[] }> = ({ items }) => {
+  return (
+    <div className="container mx-auto">
+      <nav className="flex" aria-label="Breadcrumb">
+        <ol className="inline-flex items-center space-x-1 md:space-x-3">
+          {items.map((breadcrumb, index) => (
+            <BreadcrumbItem
+              key={index}
+              showDelimiter={index > 0}
+              breadcrumb={breadcrumb}
+            />
+          ))}
+        </ol>
+      </nav>
+    </div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,14 @@
+import { Breadcrumbs } from '../components/Breadcrumbs'
+
 export default function Home() {
-  return <main>content</main>
+  return (
+    <main>
+      <Breadcrumbs
+        items={['Service Canada', 'OAS/GIS Account', 'Personal Information']}
+      />
+      <div className="container mx-auto flex flex-col mt-8 font-gc space-y-4 mb-16">
+        <h1 className="h1 mb-10 border-b border-b-[#892f35]">Welcome, Sidra</h1>
+      </div>
+    </main>
+  )
 }
