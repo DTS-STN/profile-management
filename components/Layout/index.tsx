229e796a8050414d58aadbc098028a56a136ca45
Khalid <kadan@deloitte.ca>
2021-12-03 13:39:15 -0500

    Added better looking header and footer

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -5,6 +5,7 @@ import { LanguageContext } from '../Contexts'
 import { useInternationalization } from '../Hooks'
 import { NavigationHeader } from '../NavigationHeader'
 import { Footer } from './Footer'
+import { Header } from './Header'
 
 export const Layout: React.VFC<{
   children: React.ReactNode
@@ -20,15 +21,26 @@ export const Layout: React.VFC<{
   return (
     <main>
       <div className="mx-4 min-h-screen">
-        <div className="container mx-auto flex flex-col mb-16">
+        <div className="container mx-auto">
           <div className="flex justify-end my-4">
             <button
-              className="btn-link btn"
+              className="btn-link btn underline"
               onClick={(e) => userLanguageChange(otherLang)}
             >
               {otherLangNameFull}
             </button>
           </div>
+        </div>
+        <Header />
+        <div className="bg-primary -mx-4">
+          <div className="flex flex-row justify-between items-center container mx-auto">
+            <h3 className="text-h3 py-3 text-white font-bold">
+              Service Canada
+            </h3>
+            <p className="font-bold text-white">Sign out</p>
+          </div>
+        </div>
+        <div className="container mx-auto flex flex-col mb-16">
           <Breadcrumbs items={['Service Canada', 'OAS/GIS Account', title]} />
           <h1 className="h1 my-8 mb-10">
             {useInternationalization('welcome')}, {`${data.firstName}`}
